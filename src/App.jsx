1b521155d8893311506800ca11db491956f6730b
Stefan Bettaglio <sbettaglio@gmail.com>
2020-03-29 18:16:23 -0400

    basic html user home page

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import { BrowserRouter as Router, Link, Route, Switch } from 'react-router-dom'
-import Page from './pages/Page'
+import UserHome from './pages/UserHome'
 import Register from './pages/Register'
 import HomePage from './pages/HomePage'
 import NotFound from './pages/NotFound'
@@ -18,7 +18,7 @@ const App = () => {
               <Link to="/register">Register</Link>
             </li>
             <li>
-              <Link to="/2">Search</Link>
+              <Link to="/userHome">User Home</Link>
             </li>
           </ul>
         </nav>
@@ -26,6 +26,7 @@ const App = () => {
       <Switch>
         <Route exact path="/" component={HomePage}></Route>
         <Route exact path="/register" component={Register}></Route>
+        <Route exact path="/userHome" component={UserHome}></Route>
         <Route path="*" component={NotFound}></Route>
       </Switch>
     </Router>
---
+++ src/pages/UserHome.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+
+const UserHome = () => {
+  return (
+    <>
+      <div>
+        <h1>User Home Page</h1>
+      </div>
+      <main>
+        <ul>
+          <li>Today's Picks</li>
+          <li>Today's Drops</li>
+          <li>Late Trucks</li>
+          <li>Carrier Profiles</li>
+        </ul>
+        <button>Logout</button>
+      </main>
+    </>
+  )
+}
+
+export default UserHome
