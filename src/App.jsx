ec68a946c4ff2a8884fa5bf9a05d317d7c84debc
Stefan Bettaglio <sbettaglio@gmail.com>
2020-03-29 17:08:38 -0400

    basic html home

---
+++ src/App.jsx
@@ -0,0 +1,36 @@
+import React from 'react'
+import { BrowserRouter as Router, Link, Route, Switch } from 'react-router-dom'
+import Page from './pages/Page'
+import Page2 from './pages/Page2'
+import HomePage from './pages/HomePage'
+import NotFound from './pages/NotFound'
+
+const App = () => {
+  return (
+    <Router>
+      <header>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/1">Sign Up</Link>
+            </li>
+            <li>
+              <Link to="/2">Search</Link>
+            </li>
+          </ul>
+        </nav>
+      </header>
+      <Switch>
+        <Route exact path="/" component={HomePage}></Route>
+        <Route exact path="/1" component={Page}></Route>
+        <Route exact path="/2" component={Page2}></Route>
+        <Route path="*" component={NotFound}></Route>
+      </Switch>
+    </Router>
+  )
+}
+
+export default App
---
+++ src/pages/HomePage.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+
+const HomePage = () => {
+  return (
+    <>
+      <main>
+        <h1>Truck Tracker</h1>
+        <form>
+          <label>Username</label>
+          <br></br>
+          <input type="text"></input>
+          <br></br>
+          <label>Password</label>
+          <br></br>
+          <input type="password"></input>
+        </form>
+      </main>
+    </>
+  )
+}
+
+export default HomePage
